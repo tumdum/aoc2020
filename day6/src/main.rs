c25e64ae8ea75eb8dffe08f0486c1a5344edc7d5
Tomasz Kłak <tomasz@tomaszklak.pl>
2020-12-06 11:19:07 +0100

    Simplified day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,36 +1,27 @@
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
 use std::io::BufRead;
+type Set = std::collections::HashSet<char>;
 
-fn merge(l: Vec<HashSet<char>>) -> Vec<Vec<HashSet<char>>> {
-    l.split(HashSet::is_empty).map(|s| s.to_vec()).collect()
-}
-
-fn sum_any(input: &[HashSet<char>]) -> HashSet<char> {
-    input
-        .iter()
-        .cloned()
-        .fold1(|a, b| a.into_iter().chain(b.into_iter()).collect())
-        .unwrap()
-}
-
-fn sum_all(input: &[HashSet<char>]) -> HashSet<char> {
-    input
-        .iter()
-        .cloned()
-        .fold1(|a, b| a.intersection(&b).cloned().collect())
-        .unwrap()
+fn sum(input: &[Set], f: impl FnMut(Set, Set) -> Set) -> usize {
+    input.iter().cloned().fold1(f).map_or(0, |s| s.len())
 }
 
 fn main() {
-    let input = merge(
-        std::io::stdin()
-            .lock()
-            .lines()
-            .map(|l| l.unwrap().chars().collect::<HashSet<_>>())
-            .collect::<Vec<_>>(),
-    );
+    let input: Vec<Vec<Set>> = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|l| l.unwrap().chars().collect::<Set>())
+        .collect::<Vec<_>>()
+        .split(Set::is_empty)
+        .map(|s| s.to_vec())
+        .collect();
 
-    dbg!(input.iter().map(|s| sum_any(s).len()).sum::<usize>());
-    dbg!(input.iter().map(|s| sum_all(s).len()).sum::<usize>());
+    dbg!(input
+        .iter()
+        .map(|s| sum(s, |a, b| a.into_iter().chain(b.into_iter()).collect()))
+        .sum::<usize>());
+    dbg!(input
+        .iter()
+        .map(|s| sum(s, |a, b| a.intersection(&b).cloned().collect()))
+        .sum::<usize>());
 }
