c3f7ccb254644bbfc86b25da80c24e389e319f24
Tomasz Kłak <tomasz@tomaszklak.pl>
2020-12-19 10:31:27 +0100

    Day 19 cleanup

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -2,30 +2,29 @@ use std::collections::HashMap;
 use std::io::BufRead;
 
 #[derive(Debug, Clone, PartialEq)]
-enum Rule {
-    Lit(String),
+enum Rule<'a> {
+    Lit(&'a str),
     Seq(Vec<usize>),
     Alt(Vec<usize>, Vec<usize>),
 }
 
 fn parse(s: &str) -> (usize, Rule) {
     let sp = s.split(": ").collect::<Vec<_>>();
-    let id = sp[0].parse().unwrap();
     let rules = sp[1];
-    if rules.starts_with('"') {
-        (id, Rule::Lit(rules[1..2].to_owned()))
-    } else if rules.contains('|') {
-        let v: Vec<Vec<_>> = rules
-            .split('|')
-            .map(|s| s.trim().split(' ').map(|s| s.parse().unwrap()).collect())
-            .collect();
-        (id, Rule::Alt(v[0].clone(), v[1].clone()))
-    } else {
-        (
-            id,
-            Rule::Seq(rules.split(' ').map(|s| s.parse().unwrap()).collect()),
-        )
-    }
+    (
+        sp[0].parse().unwrap(),
+        if rules.starts_with('"') {
+            Rule::Lit(&rules[1..2])
+        } else if rules.contains('|') {
+            let v: Vec<Vec<_>> = rules
+                .split('|')
+                .map(|s| s.trim().split(' ').map(|s| s.parse().unwrap()).collect())
+                .collect();
+            Rule::Alt(v[0].clone(), v[1].clone())
+        } else {
+            Rule::Seq(rules.split(' ').map(|s| s.parse().unwrap()).collect())
+        },
+    )
 }
 
 fn matching_tails_seq<'a, 'b>(
@@ -54,7 +53,9 @@ fn matching_tails<'a, 'b>(r: &'a Rule, input: &'b str, all: &HashMap<usize, Rule
 }
 
 fn matches(s: &str, all: &HashMap<usize, Rule>) -> bool {
-    matching_tails(&all[&0], s, all).iter().any(|m| *m == "")
+    matching_tails(&all[&0], s, all)
+        .into_iter()
+        .any(str::is_empty)
 }
 
 fn main() {
@@ -78,7 +79,7 @@ fn main() {
 
 #[test]
 fn tests() {
-    assert_eq!((3, Rule::Lit("b".to_owned())), parse("3: \"b\""));
+    assert_eq!((3, Rule::Lit("b")), parse("3: \"b\""));
     assert_eq!((0, Rule::Seq(vec![1, 2])), parse("0: 1 2"));
     assert_eq!(
         (2, Rule::Alt(vec![1, 3], vec![3, 1])),
@@ -86,7 +87,7 @@ fn tests() {
     );
     assert_eq!(
         vec!["b"],
-        matching_tails(&Rule::Lit("a".to_owned()), "ab", &HashMap::new())
+        matching_tails(&Rule::Lit("a"), "ab", &HashMap::new())
     );
-    assert!(matching_tails(&Rule::Lit("a".to_owned()), "bab", &HashMap::new()).is_empty());
+    assert!(matching_tails(&Rule::Lit("a"), "bab", &HashMap::new()).is_empty());
 }
