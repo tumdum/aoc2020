5a5def804f4b30f0ebde4c4759bca997820cacf1
Tomasz Kłak <tomasz@tomaszklak.pl>
2020-12-23 10:22:42 +0100

    map to vec

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
-type M = std::collections::HashMap<u32, u32>;
+type M = Vec<usize>;
 
-fn destination(mut current: u32, min: u32, max: u32, a: u32, b: u32, c: u32) -> u32 {
+fn destination(mut current: usize, min: usize, max: usize, a: usize, b: usize, c: usize) -> usize {
     loop {
         current -= 1;
         if current < min {
@@ -13,38 +13,41 @@ fn destination(mut current: u32, min: u32, max: u32, a: u32, b: u32, c: u32) -> 
     }
 }
 
-fn take_next(mut from: u32, m: &M, count: usize) -> Vec<u32> {
+fn take_next(mut from: usize, m: &M, count: usize) -> Vec<usize> {
     let mut ret = vec![];
     while ret.len() < count {
-        from = m[&from];
+        from = m[from];
         ret.push(from);
     }
     ret
 }
 
-fn solve_fast(input: Vec<u32>, moves: usize) -> (u64, String) {
+fn solve_fast(input: Vec<usize>, moves: usize) -> (u64, String) {
     let min = *input.iter().min().unwrap();
     let max = *input.iter().max().unwrap();
-    let mut map: M = input.windows(2).map(|w| (w[0], w[1])).collect();
-    map.insert(*input.last().unwrap(), input[0]);
+    let mut next: Vec<usize> = vec![0; input.len() + 1];
+    for v in input.windows(2) {
+        next[v[0]] = v[1];
+    }
+    next[*input.last().unwrap()] = input[0];
     let mut current = input[0];
     for _ in 1..=moves {
-        let pick = take_next(current, &map, 4);
+        let pick = take_next(current, &next, 4);
         let dest = destination(current, min, max, pick[0], pick[1], pick[2]);
-        let dest_next = map[&dest];
-        map.insert(dest, pick[0]);
-        map.insert(pick[2], dest_next);
-        map.insert(current, pick[3]);
+        let dest_next = next[dest];
+        next[dest] = pick[0];
+        next[pick[2]] = dest_next;
+        next[current] = pick[3];
         current = pick[3];
     }
-    let tmp = take_next(1, &map, 8);
+    let tmp = take_next(1, &next, 8);
     let prod = tmp[0] as u64 * tmp[1] as u64;
     (prod, format!("{}", tmp.into_iter().format("")))
 }
 
-fn make_big_input(start: &[u32], max: u32) -> Vec<u32> {
+fn make_big_input(start: &[usize], max: usize) -> Vec<usize> {
     let next = *start.iter().max().unwrap() + 1;
-    let mut input: Vec<u32> = start.iter().cloned().collect();
+    let mut input: Vec<usize> = start.iter().cloned().collect();
     input.extend(next..=max);
     input
 }
